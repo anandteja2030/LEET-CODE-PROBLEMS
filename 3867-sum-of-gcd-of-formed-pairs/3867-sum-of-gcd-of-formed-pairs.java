ed4966c5c09e517a6a23db2232857248e22e95e2
Anand Teja Dasari <anandtejadasari52@gmail.com>
2026-07-17 10:01:11 +0530

    Refactor gcdSum to gcdValues for pairwise GCDs

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -1,31 +1,25 @@
 class Solution {
+
     static int gcd(int a , int b){
         if(b == 0)
             return a;
         return gcd(b , a%b);
     }
-    public long gcdSum(int[] nums) {
+    public int[] gcdValues(int[] nums, long[] q) {
+        List<Integer> list =  new ArrayList<>();
 
+        for(int i =0  ; i<nums.length-1 ; i++){
 
-        int pref[] = new int[nums.length];
-        int max = nums[0];
-        pref[0] = gcd(nums[0], max);
-
-        for(int i =1 ; i<nums.length ; i++){
-            max = Math.max(max , nums[i]);
-            pref[i] = gcd(nums[i] , max);
+            for(int j = i+1 ; j<nums.length ; j++)
+                 list.add(gcd(nums[i] , nums[j]));
+            
         }
+        Collections.sort(list);
+        int res[] = new int[q.length];
 
-       Arrays.sort(pref);
-       long sum =0 ;
-       int left =0 ;
-       int right = pref.length -1;
-
-       while(left < right)
-        sum += gcd(pref[left++] , pref[right--]);
-
-    return sum;
-       
-
-    }
+        for(int i =0 ; i<res.length ; i++)
+            res[i] = list.get((int)q[i]);
+        
+        return res;
+    }+
 }
