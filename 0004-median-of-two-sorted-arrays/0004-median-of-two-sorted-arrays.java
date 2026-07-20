00974d7874cc96fd70e88f1c172e1a16b2c72e99
Anand Teja Dasari <anandtejadasari52@gmail.com>
2026-07-20 11:32:45 +0530

    Time: 7 ms (23.95%), Space: 49.1 MB (5.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,34 +1,15 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-
-        int total = n + m;
-        int mid = total / 2;
-
-        int i = 0, j = 0;
-        int count = 0;
-
-        int prev = 0, curr = 0;
-
-        while (count <= mid) {
-            prev = curr;
-
-            if (i < n && (j >= m || nums1[i] <= nums2[j])) {
-                curr = nums1[i];
-                i++;
-            } else {
-                curr = nums2[j];
-                j++;
-            }
-
-            count++;
+        int merge[]=new int[nums1.length+nums2.length];
+        System.arraycopy(nums1 , 0 , merge , 0 , nums1.length);
+        System.arraycopy(nums2 , 0 , merge , nums1.length , nums2.length);
+        Arrays.sort(merge);
+        int len=merge.length;
+            if(len==2)
+                return (double) (merge[0]+merge[1])/2;
+            else if(len >2 && len%2==0)
+                return (double) (merge[(len/2)-1]+merge[(len/2)])/2;
+            else 
+                return (double) merge[len/2];
         }
-
-        if (total % 2 == 0) {
-            return (prev + curr) / 2.0;
-        }
-
-        return curr;
     }
-}
